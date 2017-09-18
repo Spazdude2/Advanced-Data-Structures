dbd8544420289637ba7c9cc76cf961a465fed9b3
Spazdude2 <spaziani96@gmail.com>
2017-09-17 23:29:05 -0400

    made granade launcher

--- Class Tutorial/Assets/Player/PlayerMovement.cs
+++ Class Tutorial/Assets/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float Speed;
     public GameObject shot;
+	public Transform GunEnd;
+	public float BulletSpeed;
 
 	// Use this for initialization
 	void Start ()
@@ -29,8 +31,8 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject newShot = Instantiate(shot, Camera.main.transform.position + Camera.main.transform.forward * 0.5f, Quaternion.Euler(transform.rotation.eulerAngles));
-            newShot.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * 12;
+            GameObject newShot = Instantiate(shot, GunEnd.transform.position + GunEnd.transform.forward * 0.5f, Quaternion.Euler(transform.rotation.eulerAngles));
+            newShot.GetComponent<Rigidbody>().velocity = GunEnd.transform.forward * BulletSpeed;
  
         }
 	}
